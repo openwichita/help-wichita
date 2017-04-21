3a84838cef5b2a0c67b7cc35f0a94fd941a051e8
Seth Etter <sethetter@gmail.com>
2017-04-21 14:06:29 -0500

    Basic twitter bot, RT certain hashtag

---
+++ README.md
@@ -0,0 +1,14 @@
+# Help Wichita
+
+An aggregate of opportunities to help in Wichita.
+
+## Initial Plan
+
+* Start with a twitter bot to automagically retweet anything with the hashtag #helpict
+* Create a web frontend to pull these same posts and display them nicely
+
+## Future Ideas
+
+* Create Facebook integration
+* More information on specific organizations
+* Have other ideas? Post an issue!
---
+++ services/twitter.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const Twitter = require('twitter')
+const moment = require('moment')
+const debug = require('debug')('help-wichita:twitter')
+
+const HASHTAG = process.env.HASHTAG
+const CHECK_INTERVAL = process.env.CHECK_INTERVAL || 300000
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret:process.env.ACCESS_TOKEN_SECRET
+})
+
+client.get('search/tweets', {
+  q: `#${HASHTAG} since:${moment().format('YYYY-MM-DD')}`,
+}).then(data => {
+  data.statuses.forEach(retweetStatus)
+}).catch(debug)
+
+function retweetStatus(status) {
+  client.post('statuses/retweet', {
+    id: status.id_str
+  }).then((retweetedStatus, response) => {
+    debug(`-- Retweeting status ${status.id_str} from ${retweetStatus.screen_name}`);
+  }).catch(debug);
+}
