153d2d9daca1ce3b23292be3ea4d66c440e97d98
Seth Etter <sethetter@gmail.com>
2017-04-22 08:00:58 -0500

    Only need production right now

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,26 +20,14 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
   deploy : {
-    //production : {
-    //  user : 'node',
-    //  host : '212.83.163.1',
-    //  ref  : 'origin/master',
-    //  repo : 'git@github.com:repo.git',
-    //  path : '/var/www/production',
-    //  'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-    //  'post-setup': 'cp .env.example ../shared/.env && ln -s ../shared/.env ../current/.env'
-    //},
-    dev : {
+    production : {
       user : 'seth',
       host : '192.241.221.238',
       ref  : 'origin/master',
       repo : 'git@github.com:openwichita/help-wichita.git',
       path : '/home/seth/deploys/help-wichita',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      'post-setup': 'cp .env.example ../shared/.env && ln -s ../shared/.env ../current/.env',
-      env  : {
-        NODE_ENV: 'dev'
-      }
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production',
+      'post-setup': 'cp .env.example ../shared/.env && ln -s ../shared/.env ../current/.env'
     }
   }
 }
